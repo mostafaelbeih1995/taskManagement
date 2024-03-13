0347848d70b1655bd934c13ac0293449956fad65
mostafaelbeih <mostafa.elbeihjr@gmail.com>
2024-03-13 20:32:41 +0200

    Task Management: First commit

---
+++ src/main/java/banquemisr/challenge05/Challenge05Application.java
@@ -0,0 +1,13 @@
+package banquemisr.challenge05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge05Application.class, args);
+	}
+
+}
